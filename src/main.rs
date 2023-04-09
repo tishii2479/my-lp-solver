983fb20062b77dfa58431d70f6e08e32383cbbc5
Tatsuya Ishii <tishii2479@gmail.com>
2023-04-09 13:47:03 +0900

    refactor parse_lp_file_old to parse_lp_file and tokenize_lp_file, and delete old parser

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,87 @@
 use std::collections::HashMap;
 
+#[derive(Debug)]
+enum Token {
+    // NOTE: TokenとKeywordを一緒にしたら楽じゃない?
+    Const { val: f64 },
+    Variable { name: String },
+    Name { val: String },
+    Keyword { val: Keyword },
+}
+
 #[derive(Clone, Copy, PartialEq, Debug)]
-enum Compare {
+enum Keyword {
+    Maximize,
+    Minimize,
+    SubjectTo,
+    Bounds,
+    Free,
+    General,
+    Binary,
+    End,
     LessEqual,
     Equal,
     GreaterEqual,
+    Plus,
+    Minus,
 }
 
-impl Compare {
+impl Keyword {
     fn to_str(&self) -> &str {
         match self {
-            Compare::LessEqual => "<=",
-            Compare::Equal => "=",
-            Compare::GreaterEqual => ">=",
+            Keyword::Maximize => "maximize",
+            Keyword::Minimize => "minimize",
+            Keyword::SubjectTo => "st",
+            Keyword::Bounds => "bounds",
+            Keyword::Free => "free",
+            Keyword::General => "general",
+            Keyword::Binary => "binary",
+            Keyword::End => "end",
+            Keyword::LessEqual => "<=",
+            Keyword::Equal => "=",
+            Keyword::GreaterEqual => ">=",
+            Keyword::Plus => "+",
+            Keyword::Minus => "-",
         }
     }
 
-    fn inv(&self) -> Compare {
+    fn is_same(&self, s: &str) -> bool {
         match self {
-            Compare::Equal => Compare::Equal,
-            Compare::GreaterEqual => Compare::LessEqual,
-            Compare::LessEqual => Compare::GreaterEqual,
+            _ => s == self.to_str(),
         }
+    }
+
+    fn matched_keyword(s: &str) -> Option<Keyword> {
+        for keyword in Keyword::all() {
+            if keyword.is_same(s) {
+                return Some(keyword);
+            }
+        }
+        None
+    }
+
+    fn all() -> impl Iterator<Item = Keyword> {
+        [
+            Keyword::Maximize,
+            Keyword::Minimize,
+            Keyword::SubjectTo,
+            Keyword::Bounds,
+            Keyword::Free,
+            Keyword::General,
+            Keyword::Binary,
+            Keyword::End,
+            Keyword::LessEqual,
+            Keyword::Equal,
+            Keyword::GreaterEqual,
+            Keyword::Plus,
+            Keyword::Minus,
+        ]
+        .iter()
+        .copied()
     }
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 struct Term {
     var_idx: usize,
     coef: f64,
@@ -49,11 +105,11 @@ impl Term {
     }
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct Constraint {
     name: String,
     expr: Vec<Term>,
-    cmp: Compare,
+    cmp: Keyword,
     rhs: f64,
 }
 
@@ -126,29 +182,29 @@ impl Problem {
 
         let mut constraints = vec![];
         for c in self.constraints.iter() {
-            if c.cmp != Compare::Equal {
+            if c.cmp != Keyword::Equal {
                 constraints.push(c.clone());
                 continue;
             }
             constraints.push(Constraint {
                 name: c.name.to_owned() + "_1",
                 expr: c.expr.clone(),
-                cmp: Compare::LessEqual,
+                cmp: Keyword::LessEqual,
                 rhs: c.rhs,
             });
             constraints.push(Constraint {
                 name: c.name.to_owned() + "_2",
                 expr: c.expr.clone(),
-                cmp: Compare::GreaterEqual,
+                cmp: Keyword::GreaterEqual,
                 rhs: c.rhs,
             });
         }
         for c in &mut constraints {
-            if c.cmp != Compare::GreaterEqual {
+            if c.cmp != Keyword::GreaterEqual {
                 continue;
             }
             c.expr.iter_mut().for_each(|t| t.coef *= -1.);
-            c.cmp = Compare::LessEqual;
+            c.cmp = Keyword::LessEqual;
             c.rhs *= -1.;
         }
 
@@ -162,9 +218,9 @@ impl Problem {
 
     fn output(&self) {
         if self.is_maximize {
-            println!("maximize");
+            println!("{}", Keyword::Maximize.to_str());
         } else {
-            println!("minimize");
+            println!("{}", Keyword::Minimize.to_str());
         }
 
         print!("obj: ");
@@ -173,7 +229,7 @@ impl Problem {
         }
         println!();
 
-        println!("subject to");
+        println!("{}", Keyword::SubjectTo.to_str());
 
         for constraint in self.constraints.iter() {
             println!(
@@ -183,7 +239,7 @@ impl Problem {
             );
         }
 
-        println!("end");
+        println!("{}", Keyword::End.to_str());
     }
 }
 
@@ -375,143 +431,255 @@ impl Solver for SimplexLpSolver {
     }
 }
 
-fn parse_lp_file(lp: &str) -> Problem {
-    let mut lines = lp.lines();
-    let problem_objective = lines.next().unwrap().trim().to_lowercase();
-    assert!(
-        problem_objective == "maximize" || problem_objective == "minimize",
-        "problem objective should be `maximize` or `minimize`, actual value: {}",
-        problem_objective
-    );
-    let is_maximize = problem_objective == "maximize";
+fn tokenize_lp_file(lp: &str) -> Vec<Token> {
+    let mut tokens = vec![];
+    let mut q = lp.split_whitespace().peekable();
 
-    // 目的関数のパース
-    let mut it = lines.next().unwrap().trim().split(" ").peekable();
-    if it.peek().unwrap_or(&"obj:").to_lowercase() == "obj:" {
-        it.next();
-    }
+    while let Some(s) = q.next() {
+        // キーワードの検知
+        if let Some(keyword) = Keyword::matched_keyword(s) {
+            tokens.push(Token::Keyword { val: keyword });
+            continue;
+        }
 
-    let mut objective = vec![];
-    let mut variables = vec![];
-    let mut var_map = HashMap::new();
+        // 名前の検知
+        if s.ends_with(":") {
+            tokens.push(Token::Name {
+                val: s[0..s.len() - 1].to_owned(),
+            });
+            continue;
+        }
+        // waiting for if-let chain...
+        if q.peek().is_some() && q.peek().unwrap() == &":" {
+            tokens.push(Token::Name { val: s.to_owned() });
+            q.next();
+            continue;
+        }
 
-    while let Some(mut t) = it.next() {
-        let sign = if t == "-" {
-            t = it.next().unwrap();
-            -1.
-        } else if t == "+" {
-            t = it.next().unwrap();
-            1.
-        } else if t.parse::<f64>().is_ok() && objective.len() == 0 {
-            // 目的関数の最初の項は、+がなくても正の値と解釈する
-            1.
+        // NOTE: 1xのように、スペースで区切られていない場合は対応していない
+        let starts_with_digit = s.chars().next().unwrap().is_digit(10);
+        if starts_with_digit {
+            // 定数をパースする
+            tokens.push(Token::Const {
+                val: s.parse().unwrap(),
+            });
         } else {
-            panic!("unexpected str: {}", t);
-        };
-        let coef_abs = t.parse::<f64>().unwrap();
-        let coef = sign * coef_abs;
-        let var_name = it.next().unwrap();
-        let var_idx = var_map.len();
-        objective.push(Term { var_idx, coef });
-
-        if !var_map.contains_key(var_name) {
-            var_map.insert(var_name, var_idx);
-            variables.push(Variable {
-                name: var_name.to_string(),
-                var_type: VariableType::Real,
-                is_free: false,
-            })
+            // 変数をパースする
+            tokens.push(Token::Variable { name: s.to_owned() });
         }
     }
 
-    // 制約のパース
-    assert_eq!(lines.next().unwrap().trim().to_lowercase(), "subject to");
+    tokens
+}
+
+fn parse_name(q: &mut std::iter::Peekable<core::slice::Iter<Token>>, default_name: &str) -> String {
+    match q.peek() {
+        Some(Token::Name { val }) => {
+            q.next();
+            val.to_owned()
+        }
+        _ => default_name.to_owned(),
+    }
+}
+
+fn parse_constant(q: &mut std::iter::Peekable<core::slice::Iter<Token>>) -> Option<f64> {
+    if let Some(Token::Const { val }) = q.peek() {
+        q.next();
+        Some(*val)
+    } else {
+        None
+    }
+}
+
+fn parse_variable(
+    q: &mut std::iter::Peekable<core::slice::Iter<Token>>,
+    variable_map: &mut HashMap<String, usize>,
+) -> usize {
+    let Some(Token::Variable { name: var_name }) = q.next() else {
+        panic!("expected variable name");
+    };
+    return if variable_map.contains_key(var_name) {
+        variable_map[var_name]
+    } else {
+        let idx = variable_map.len();
+        variable_map.insert(var_name.clone(), idx);
+        idx
+    };
+}
+
+fn parse_expr(
+    q: &mut std::iter::Peekable<core::slice::Iter<Token>>,
+    variable_map: &mut HashMap<String, usize>,
+) -> Vec<Term> {
+    let mut expr = vec![];
+    while let Some(tok) = q.peek() {
+        match tok {
+            Token::Keyword { val } => {
+                match val {
+                    Keyword::Minus | Keyword::Plus => {
+                        let sign = if *val == Keyword::Minus { -1. } else { 1. };
+                        q.next();
+                        let coef = parse_constant(q).unwrap_or(1.) * sign;
+                        let var_idx = parse_variable(q, variable_map);
+                        expr.push(Term { var_idx, coef });
+                    }
+                    _ => {
+                        return expr;
+                    }
+                };
+            }
+            Token::Variable { name: _ } => {
+                // 符号なし、係数なしの項は式の最初しか認めない
+                if expr.len() != 0 {
+                    panic!("unexpected token: {:?}", tok);
+                }
+                let var_idx = parse_variable(q, variable_map);
+                expr.push(Term { var_idx, coef: 1. })
+            }
+            Token::Const { val: coef } => {
+                // 符号なしの項は式の最初しか認めない
+                if expr.len() != 0 {
+                    panic!("unexpected token: {:?}", tok);
+                }
+                q.next();
+                let var_idx = parse_variable(q, variable_map);
+                expr.push(Term {
+                    var_idx,
+                    coef: *coef,
+                })
+            }
+            _ => {
+                panic!("unexpected token: {:?}", tok);
+            }
+        }
+    }
+    expr
+}
+
+fn parse_objective(
+    q: &mut std::iter::Peekable<core::slice::Iter<Token>>,
+    variable_map: &mut HashMap<String, usize>,
+) -> Vec<Term> {
+    let obj_name = parse_name(q, "obj");
+    let expr = parse_expr(q, variable_map);
+    expr
+}
+
+fn parse_constraint(
+    q: &mut std::iter::Peekable<core::slice::Iter<Token>>,
+    variable_map: &mut HashMap<String, usize>,
+    default_name: &str,
+) -> Constraint {
+    let c_name = parse_name(q, default_name);
+    let expr = parse_expr(q, variable_map);
+    let cmp = match q.next().unwrap() {
+        Token::Keyword { val } => match val {
+            Keyword::GreaterEqual | Keyword::Equal | Keyword::LessEqual => val,
+            _ => {
+                panic!("unexpected keyword: {:?}", val);
+            }
+        },
+        _ => {
+            panic!("unexpected token");
+        }
+    };
+    let rhs = parse_constant(q).expect("expected constant");
+
+    Constraint {
+        name: c_name,
+        expr,
+        cmp: *cmp,
+        rhs,
+    }
+}
+
+fn parse_constraints(
+    q: &mut std::iter::Peekable<core::slice::Iter<Token>>,
+    variable_map: &mut HashMap<String, usize>,
+) -> Vec<Constraint> {
     let mut constraints = vec![];
-    for line in lines {
-        if line.trim().to_lowercase() == "end" {
-            break;
+    while let Some(tok) = q.peek() {
+        match tok {
+            Token::Keyword { val: _ } => {
+                return constraints;
+            }
+            _ => {
+                let c = parse_constraint(q, variable_map, &format!("c{}", constraints.len()));
+                constraints.push(c);
+            }
         }
-        let mut it = line.trim().split(" ").peekable();
-        // 名前は飛ばす
-        let name = if it.peek().unwrap_or(&"na").to_lowercase().ends_with(":") {
-            let a = it.next().unwrap();
-            a[0..a.len() - 1].to_owned()
-        } else {
-            format!("c{}", constraints.len() + 1)
-        };
+    }
+    constraints
+}
 
-        let mut expr = vec![];
-        let mut cmp = None;
-        while let Some(mut t) = it.next() {
-            let sign = if t == "-" {
-                t = it.next().unwrap();
-                -1.
-            } else if t == "+" {
-                t = it.next().unwrap();
-                1.
-            } else if t.parse::<f64>().is_ok() && expr.len() == 0 {
-                // 目的関数の最初の項は、+がなくても正の値と解釈する
-                1.
-            } else if t == Compare::LessEqual.to_str() {
-                cmp = Some(Compare::LessEqual);
-                break;
-            } else if t == Compare::Equal.to_str() {
-                cmp = Some(Compare::Equal);
-                break;
-            } else if t == Compare::GreaterEqual.to_str() {
-                cmp = Some(Compare::GreaterEqual);
-                break;
-            } else {
-                panic!("unexpected str: {}", t);
-            };
-            let coef_abs = t.parse::<f64>().unwrap();
-            let coef = sign * coef_abs;
-            let var_name = it.next().unwrap();
-            let var_idx = var_map[var_name];
-            expr.push(Term { var_idx, coef });
+fn parse_lp_file(tokens: &Vec<Token>) -> Problem {
+    let mut q = tokens.iter().peekable();
+    let mut variable_map: HashMap<String, usize> = HashMap::new();
+    let mut is_maximize = None;
+    let mut objective = None;
+    let mut constraints = None;
+
+    // NOTE: bounds, general, binaryは未実装
+    while let Some(tok) = q.next() {
+        match tok {
+            Token::Keyword { val } => match *val {
+                Keyword::Maximize | Keyword::Minimize => {
+                    is_maximize = Some(*val == Keyword::Maximize);
+                    objective = Some(parse_objective(&mut q, &mut variable_map));
+                }
+                Keyword::SubjectTo => {
+                    constraints = Some(parse_constraints(&mut q, &mut variable_map));
+                }
+                Keyword::End => break,
+                _ => panic!("unexpected keyword {}", val.to_str()),
+            },
+            _ => panic!("unexpected token: {:?}", tok),
         }
-        let cmp = cmp.unwrap();
-        let rhs = it.next().unwrap().parse::<f64>().unwrap();
-        constraints.push(Constraint {
-            name,
-            expr,
-            cmp,
-            rhs,
-        })
+    }
+
+    let mut variables = vec![];
+    for (var_name, var_idx) in variable_map {
+        variables.push(Variable {
+            name: var_name,
+            var_type: VariableType::Real,
+            is_free: false,
+        });
     }
 
     Problem {
-        is_maximize,
+        is_maximize: is_maximize.expect("could not parse problem objective"),
         variables,
-        objective,
-        constraints,
+        objective: objective.expect("could not parse objective"),
+        constraints: constraints.expect("could not parse constraints"),
     }
 }
 
 fn main() {
-    let problem = parse_lp_file(
+    let tokens = tokenize_lp_file(
         "maximize
 obj: 5 x + 4 y
-subject to
-1.5 x + 3 y <= 13.5
-3 x + 1 y <= 10
+st
+c1: 1.5 x + 3 y <= 13.5
+c2: 3 x + 1 y <= 10
 end
 ",
     );
+    let problem = parse_lp_file(&tokens);
     problem.output();
 }
 
 #[test]
 fn test_simple_lp_problem() {
-    let problem = parse_lp_file(
+    let tokens = tokenize_lp_file(
         "maximize
 obj: 5 x + 4 y
-subject to
-1.5 x + 3 y <= 13.5
-3 x + 1 y <= 10
+st
+c1: 1.5 x + 3 y <= 13.5
+c2: 3 x + 1 y <= 10
 end
 ",
     );
+    let problem = parse_lp_file(&tokens);
     let solver = SimplexLpSolver;
     let obj = solver.solve(&problem);
 
